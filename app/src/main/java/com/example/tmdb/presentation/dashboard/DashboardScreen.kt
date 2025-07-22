0defd20d1de9f72a577301b21b08fafc5e58695b
asyraf-sera <muhammad.asyraf@sera.astra.co.id>
2025-07-22 10:12:23 +0700

    ver:1.0.0 msg:remove duplication top bar

--- app/src/main/java/com/example/tmdb/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/tmdb/presentation/dashboard/DashboardScreen.kt
@@ -1,9 +1,12 @@
 package com.example.tmdb.presentation.dashboard
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.tmdb.core.constant.Constants
@@ -18,6 +21,9 @@ import androidx.navigation.compose.composable
 fun DashboardScreen(navController: NavHostController) {
     val navC = rememberNavController()
     Scaffold(
+        topBar = {
+            Spacer(Modifier.height(0.dp))
+        },
         // Bottom navigation
         bottomBar = {
             BottomNavigationBar(navController = navC, bottomNavItems = Constants.BottomNavItems)
